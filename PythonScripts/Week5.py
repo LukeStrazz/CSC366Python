6e1ac77c3249ec97905c913b9ee57128f357e967
luke-angelostrazzera <lukestrazzera@gmail.com>
2024-10-10 14:51:35 -0400

    10/10

--- PythonScripts/Week5.py
+++ PythonScripts/Week5.py
@@ -1,6 +1,11 @@
 import pickle 
 from sklearn import neighbors
 from sklearn import datasets
+import matplotlib.pylab as plt
+import pandas as pd
+import numpy as np
+from sklearn import metrics
+
 knn = neighbors.KNeighborsClassifier(n_neighbors=64) #Optimal KNN size is # of neighbors. Square root of sample size. Here, sqrt(4140) = 64 neighbors. Optimum number of neighbors we should use. 
 with open('../DataFiles/dataset_small.pkl', 'rb') as file: #If this is a file we can open, procees (Meaning no restrictions). 'rb' means read binary
     data = pickle.load(file, encoding='latin1')
@@ -21,3 +26,25 @@ yhat = knn.predict(x)
 print('Prediction value of x: ' + str(yhat[-1]), ', real target: ' + str(y[-1]))
 knn.score(x,y)
 print(knn.score(x,y)) #Print the score
+
+plt.pie(np.c_[np.sum(np.where(y==1,1,0)),np.sum(np.where(y==-1,1,0))][0],
+        labels=['Not fully funded', 'Full amount'],
+        colors=['g', 'r'],
+        shadow = False,
+        autopct = '%.2f' # Floating point, %.2f would be x.xx, %.3f would be x.xxx
+        )
+plt.gcf().set_size_inches((6,6))
+plt.savefig('pie.png',dpi=300,bbox_inches='tight')
+
+# Confusion Matrix
+TP = np.sum(np.logical_and(yhat == -1, y == -1))    # True Positive
+TN = np.sum(np.logical_and(yhat == 1, y == 1))      # True Negative
+FP = np.sum(np.logical_and(yhat == -1, y == 1))     # False Positive
+FN = np.sum(np.logical_and(yhat == 1, y == -1))     # False Negative
+print('TP: ' + str(TP) + ", FP: " + str(FP))
+print('TN: ' + str(TN) + ", FN: " + str(FN))
+
+confusionMatrix = metrics.confusion_matrix(yhat, y)
+
+print("Confusion Matrix: ")
+print(confusionMatrix)
